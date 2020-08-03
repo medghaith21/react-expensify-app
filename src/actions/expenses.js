aa807dd93fd0ba0d59e729020b15627ce370b703
sleh <talbimouhamed24@gmail.com>
2020-08-03 19:04:53 +0200

    setup test database envirement

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -1,26 +1,40 @@
 import { v4 as uuidv4 } from 'uuid';
+import database from '../firebase/firebase';
 
-//Add_Expense
-export const addExpense= ({description='', note='', amount=0, createdAt=0}= {}) => ({
-    type: 'ADD_EXPENSE',
-    expense : {
-        id : uuidv4(),
-        description,
-        note,
-        amount,
-        createdAt
-    }
-})
-
-//edit expense
-export const editExpense= (id, updates) =>({
-    type : 'EDIT_EXPENSE',
-    id,
-    updates
+// ADD_EXPENSE
+export const addExpense = (expense) => ({
+  type: 'ADD_EXPENSE',
+  expense
 });
 
-//Remove_Expense
-export const removeExpense = ({id}= {}) => ({
-    type: 'REMOVE_EXPENSE',
-    id
-})
+export const startAddExpense = (expenseData = {}) => {
+  return (dispatch) => {
+    const {
+      description = '',
+      note = '',
+      amount = 0,
+      createdAt = 0
+    } = expenseData;
+    const expense = { description, note, amount, createdAt };
+
+    return database.ref('expenses').push(expense).then((ref) => {
+      dispatch(addExpense({
+        id: ref.key,
+        ...expense
+      }));
+    });
+  };
+};
+
+// REMOVE_EXPENSE
+export const removeExpense = ({ id } = {}) => ({
+  type: 'REMOVE_EXPENSE',
+  id
+});
+
+// EDIT_EXPENSE
+export const editExpense = (id, updates) => ({
+  type: 'EDIT_EXPENSE',
+  id,
+  updates
+});
